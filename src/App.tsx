dee8bd1e1cbd2f1ce48034849e94f324100839b9
Jeremiah Yerxa <Jeremiah.Yerxa@gmail.com>
2023-12-13 21:36:35 -0500

    init.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import useIntervalHook from './hooks/useIntervalHook';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const { start, stop } = useIntervalHook();
+
+    return (
+        <div style={{ padding: '20px' }}>
+            <h3>Hello</h3>
+            <div style={{
+                width: '5%',
+                display: 'flex',
+                justifyContent: 'space-between'
+            }}>
+                <button onClick={start}> start</button>
+                <button onClick={stop}>stop</button>
+            </div>
+        </div>
+    );
+};
 
 export default App;
---
+++ src/hooks/useIntervalHook.tsx
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from 'react';
+
+const useIntervalHook: () => { stop: () => void; start: () => void } = () => {
+    const knownMethod = () => {
+        console.log('hook triggered');
+    }
+    const [intervalId, setIntervalId] = useState<NodeJS.Timeout | null>(null);
+
+    const start = () => {
+        const id = setInterval(knownMethod, 1000);
+        setIntervalId(id);
+    };
+
+    const stop = () => {
+        if(intervalId) {
+            clearInterval(intervalId);
+            setIntervalId(null);
+        }
+    };
+
+    // Clean up on unmount.
+    useEffect(() => {
+        return () => {
+            if(intervalId) {
+                clearInterval(intervalId);
+            }
+        };
+    }, [intervalId]);
+
+    return { start, stop };
+};
+
+export default useIntervalHook;
